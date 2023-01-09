7865d36767612c46892ccefb0763118496b9c199
Chase Medford <chasemedford1@gmail.com>
2023-01-09 13:15:02 -0600

    CSS Positioning

--- js/sandbox-1.js
+++ js/sandbox-1.js
@@ -452,4 +452,33 @@ function hello(input){
 //     }
 // }  unfinished
 
-
+//Create a function, zipArrays, that takes two array inputs of the same length and returns an array with the elements of both arrays alternating in the order of first[0], second[0], first[1], second[1], etc. If both arrays are empty, return an empty array.
+// <!--zipArrays([], []) // returns []-->
+// <!--zipArrays([1], [2]) // returns [1, 2]-->
+// <!--zipArrays(['a', 'b'], ['c', 'd']) // returns... ['a', 'c', 'b', 'd']-->
+// <!--zipArrays([1,2,'a','b'], ['bob', null, 'sally', 25]) // returns...-->
+//
+// <!--[-->
+// <!--1,-->
+// <!--'bob',-->
+// <!--2,-->
+// <!--null,-->
+// <!--'a',-->
+// <!--'sally',-->
+// <!--'b',-->
+// <!--25-->
+// <!--]-->
+//
+const zip1 = ['a','b'];
+const zip2 = [1,2];
+function zipArrays(array1,array2){
+    let zipped = []
+    if (array1.length === 0) {
+        return [];
+    }
+    for (let i = 0; i < array1.length; i += 1){
+        zipped.push(array1[i])
+        zipped.push(array2[i])
+    }
+    return zipped;
+}
