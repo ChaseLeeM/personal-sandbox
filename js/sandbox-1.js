9f5b749cf7d112178faf014f0af700f04a8be315
Chase Medford <chasemedford1@gmail.com>
2023-01-18 09:34:29 -0600

    CSS Positioning

--- js/sandbox-1.js
+++ js/sandbox-1.js
@@ -598,3 +598,73 @@ function hello(input){
 // var clothingToWear = isColdOutside ? 'sweater and a jacket' : 'shorts and a t-shirt';
 // console.log(isColdOutside)
 
+// Write a function that takes a neighborhood object and determines if it is desirable. A neighborhood is desirable if the median home price is less than 300000, crime rates are low, and the total rating of schools is at least 24.
+
+// example data...
+
+const neighborhood1 = {
+    neighborhood: "Lovely Estates",
+    medianHomePrice: 280000,
+    pool: true,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 6},
+        {name: "High School", rating: 8}
+    ]
+};
+
+const neighborhood2 = {
+    neighborhood: "Luminous Estates",
+    medianHomePrice: 270000,
+    pool: true,
+    tennis: false,
+    crimeRate: "high",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 8},
+        {name: "High School", rating: 8}
+    ]
+}
+
+const neighborhood3 = {
+    neighborhood: "Oak Mountain",
+    medianHomePrice: 290000,
+    pool: false,
+    tennis: false,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 8},
+        {name: "Middle School", rating: 8},
+        {name: "High School", rating: 8}
+    ]
+}
+
+const neighborhood4 = {
+    neighborhood: "Ginormous Acres",
+    medianHomePrice: 350000,
+    pool: true,
+    tennis: true,
+    crimeRate: "low",
+    schools: [
+        {name: "Elementary School", rating: 9},
+        {name: "Middle School", rating: 9},
+        {name: "High School", rating: 9}
+    ]
+}
+
+function desirableNeighborhood(neighborhood){
+    if (neighborhood.medianHomePrice < 300000 && neighborhood.crimeRate === "low" && neighborhood.schools.rating >= 24){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+console.log(desirableNeighborhood(neighborhood1)) // returns false due to school rating
+console.log(desirableNeighborhood(neighborhood2)) // returns false due to crime rate
+console.log(desirableNeighborhood(neighborhood3)) // returns true
+console.log(desirableNeighborhood(neighborhood4)) // returns false due to median home price
+
